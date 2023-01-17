e7684f47cab87bea9895c8ab75abe90a289a7fca
Andy Dingley <Judge40@users.noreply.github.com>
2023-01-17 10:45:56 +0000

    feat: add dsp gateway application properties
    Add the properties required for issuing credentials to application.yml.
    Create GatewayProperties to give an easy way to inject the properties.
    
    TIS21-4066

--- src/main/java/uk/nhs/hee/tis/trainee/credentials/TisTraineeCredentialsApplication.java
+++ src/main/java/uk/nhs/hee/tis/trainee/credentials/TisTraineeCredentialsApplication.java
@@ -23,11 +23,16 @@ package uk.nhs.hee.tis.trainee.credentials;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * An application for issuing and verifying trainee digital credentials.
  */
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class TisTraineeCredentialsApplication {
 
   public static void main(String[] args) {
