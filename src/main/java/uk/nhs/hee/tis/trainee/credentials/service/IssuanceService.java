2e2915634f98d0df5211a8b509d69dfd19f1f6bb
Reuben Roberts <reuben.roberts@hee.nhs.uk>
2023-05-26 13:40:51 +0100

    fix: unused imports

--- src/main/java/uk/nhs/hee/tis/trainee/credentials/service/IssuanceService.java
+++ src/main/java/uk/nhs/hee/tis/trainee/credentials/service/IssuanceService.java
@@ -27,7 +27,6 @@ import java.time.Instant;
 import java.util.Optional;
 import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -38,7 +37,6 @@ import uk.nhs.hee.tis.trainee.credentials.dto.IssueResponseDto;
 import uk.nhs.hee.tis.trainee.credentials.mapper.CredentialMetadataMapper;
 import uk.nhs.hee.tis.trainee.credentials.model.CredentialMetadata;
 import uk.nhs.hee.tis.trainee.credentials.repository.CredentialMetadataRepository;
-import uk.nhs.hee.tis.trainee.credentials.service.GatewayService.TokenResponse;
 
 /**
  * A service providing credential issuance functionality.
